0c90aaa633e25edb66c9e9724b39d9d890259d94
zish82 <zish@hotmail.co.uk>
2022-05-26 17:50:11 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,11 +13,11 @@ namespace CSVFILE
             string csv_file_path = @"G:\CSVFileCSharp\CSV-Import-to-Database-Csharp\stock_list.csv";
             ReadCSV ReadCSV = new ReadCSV();
             InsertDatatoDB InsertDatatoDB = new InsertDatatoDB();
-            List<Stock> Stock = ReadCSV.getDataFromCSVFile(csv_file_path);
+            List<Stock> Stock = ReadCSV.getDataFromCSVFile(csv_file_path);//why not use IList<Stock> here
             InsertDatatoDB.insertDatatoDBStock(Stock);
-            GetStockList GetStockList = new GetStockList();
+            GetStockList GetStockList = new GetStockList();//you don't need to do that unless there is a requirement to save and retrieve from db, so be careful
             List<Stock> stocks = GetStockList.getStockList();
-            foreach (var item in stocks)
+            foreach (var item in stocks)//formatting is important, why this for is right after previous lines
             {
                 Console.WriteLine("{0} {1} {2}", item.Product, item.ProductCode, item.Price);
             }
